20d663711f06f5736d56862fc0c8c32d5f06204d
FIRST_NAME LAST_NAME <lorenagarcia@dyn-10-109-44-6.wless.uoregon.edu>
2023-04-21 14:36:24 -0700

    initialize

---
+++ Classexamples.js
@@ -0,0 +1,62 @@
+/* let s = [1, 2, 3, 4];
+
+console.log(s.map(myFunction))
+
+const newArray = s.map(myFunction)
+function myFunction(num)
+{
+    return num + 10;
+}
+console.log(newArray);
+
+
+const fruits = ["apple", "orange","pear","grapes"];
+fruits.forEach(myFunction);
+
+function myFunction(item)
+{
+    console.log(item);
+}
+
+
+const ages = [3, 17, 24, 32];
+const result = ages.filter(checkAge);
+function checkAge(age)
+{
+    return age >= 18;
+}
+
+console.log(result);
+
+
+const numbers = [200, 50, 30]
+console.log(numbers.reduce(myFunction));
+function myFunction (total, num)
+{
+    return total - num;
+}
+
+
+const fruits = ["orange", "apple"];
+
+let index = fruits.indexOf("orange");
+
+console.log(index);
+
+
+let text = "     Hello World      ";
+console.log(text.trim());
+*/
+
+//const x = function(a,b){return a * b};
+
+const text = '["ford", "BMW", "Chevy"]';
+const myCar = JSON.parse(text);
+console.log(myCar);
+
+var myCar = {model: "Chevy", year: 2023}
+
+var obj = {"name": "John", "age":30};
+
+
+
---
+++ lab-03-module.js
@@ -0,0 +1,34 @@
+
+//TODO Part 9 labeled in the lab-03-module.js file and follow the inline instructions to update the module.exports
+// object to export the reverseString function for import using the require() function.
+//TODO Part 10 labeled in the lab-03-module.js file and follow the inline instructions to add an 
+//anonymous function concatenateString defined within the module.exports object.
+
+const concatenateString = (string_1, string_2) => {
+    return ;
+}
+
+//part 11
+module.exports = {
+    //part 10
+    concatenateString: function(string_1, string_2){
+        return string_1 + string_2;
+    }
+    reverseString
+};
+
+
+//part 8
+function reverseString(str){
+    return [...str].reverse().join("");
+}
+
+
+
+// TODO Part 8: Reference the following website to use the spread operator
+// to reverse the string for reverseString(str) function. Comment out the
+// original return line of code.
+// https://betterprogramming.pub/5-ways-to-reverse-a-string-in-javascript-466f62845827
+function reverseString = (str) => {
+    return [str.split("").reverse().join("");]
+}
